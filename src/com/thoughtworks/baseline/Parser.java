b7cb49f6238e1b3f1d836d711d7d90c2999edba8
dsandeephegde <sandeeph@thoughtworks.com>
2015-09-19 12:16:52 +0530

    Parse method implementation which returns state 2d array

--- src/com/thoughtworks/baseline/Parser.java
+++ src/com/thoughtworks/baseline/Parser.java
@@ -12,6 +12,16 @@ public class Parser {
     }
 
     public State[][] parse() {
-        return new State[0][];
+        int size = universeString.size();
+        State[][] states = new State[size][size];
+        for (int i = 0; i < universeString.size(); i++) {
+            for (int j = 0; j < universeString.get(i).length(); j++) {
+                if(universeString.get(i).charAt(j) == 'X')
+                    states[i][j] = State.ALIVE;
+                else
+                    states[i][j] = State.DEAD;
+            }
+        }
+        return states;
     }
 }
--- test/com/thoughtworks/baseline/ParserTest.java
+++ test/com/thoughtworks/baseline/ParserTest.java
@@ -15,4 +15,19 @@ public class ParserTest {
 
         assertEquals((new State[0][]).getClass(), parser.parse().getClass());
     }
+
+    @Test
+    public void shouldReturnCorrectStateArray() {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("XX");
+        strings.add("-X");
+        Parser parser = new Parser(strings);
+        State[][] states = new State[2][2];
+        states[0][0] = State.ALIVE;
+        states[1][0] = State.DEAD;
+        states[0][1] = State.ALIVE;
+        states[1][1] = State.ALIVE;
+
+        assertEquals(states, parser.parse());
+    }
 }
